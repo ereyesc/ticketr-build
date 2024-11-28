619ca0f4e38b18b46a6d8531941d9e12cdb1650f
Erick <erick.erc.10@hotmail.com>
2024-11-28 15:49:48 -0600

    beta app done

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: 'https://definite-vulture-28.clerk.accounts.dev',
+      applicationID: 'convex',
+    },
+  ],
+};
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      { hostname: 'handsome-tern-44.convex.cloud', protocol: 'https' },
+    ],
+  },
 };
 
 export default nextConfig;
